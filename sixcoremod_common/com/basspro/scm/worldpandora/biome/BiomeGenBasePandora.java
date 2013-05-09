b720e3d9aeacb8fc53492a0449684b6a324b0488
BassPro241 <crashal96@aol.com>
2013-05-08 22:08:35 -0400

    More stuff.

--- sixcoremod_common/com/basspro/scm/worldpandora/biome/BiomeGenBasePandora.java
+++ sixcoremod_common/com/basspro/scm/worldpandora/biome/BiomeGenBasePandora.java
@@ -8,11 +8,15 @@ import net.minecraft.world.biome.BiomeGenBase;
 public class BiomeGenBasePandora extends BiomeGenBase
 {
     
-    public static final BiomeGenBase highlands = (BiomeIds.HIGHLANDS_BIOME).setColor(5596740).setBiomeName(Strings.HIGHLANDS_BIOME_NAME);
+    public static final BiomeGenBase highlands = new PandoraBiomeHighlands(BiomeIds.HIGHLANDS_BIOME).setColor(5596740).setBiomeName(Strings.HIGHLANDS_BIOME_NAME);
 
     public BiomeGenBasePandora(int par1)
     {
         super(par1);
     }
 
+    public BiomeGenBasePandora setColor(int par1)
+    {
+      return (BiomeGenBasePandora)super.setColor(par1);
+    }
 }
--- sixcoremod_common/com/basspro/scm/worldpandora/gen/layer/GenLayerPandora.java
+++ sixcoremod_common/com/basspro/scm/worldpandora/gen/layer/GenLayerPandora.java
@@ -1,6 +1,13 @@
 package com.basspro.scm.worldpandora.gen.layer;
 
-public class GenLayerPandora
-{
+import net.minecraft.world.gen.layer.GenLayer;
 
+public abstract class GenLayerPandora extends GenLayer
+{
+    public GenLayerPandora(long par1)
+    {
+      super(par1);
+    }
+    
+    public GenLayer[] makeTheWorld(long par1)
 }
