7bbc64b427db6143077cc1496cc314354ef86502
BassPro241 <crashal96@aol.com>
2013-05-14 14:37:58 -0400

    Men.

---
+++ sixcoremod_common/com/basspro/scm/worldpandora/biome/PandoraBiomePandoraVariant.java
@@ -0,0 +1,17 @@
+package com.basspro.scm.worldpandora.biome;
+
+public class PandoraBiomePandoraVariant extends BiomeGenBasePandora
+{
+
+    public PandoraBiomePandoraVariant(int par1)
+    {
+        super(par1);
+
+        this.temperature = 2.0F;
+        this.rainfall = 0.0F;
+
+        this.minHeight = -0.2F;
+        this.maxHeight = 0.4F;
+    }
+
+}
